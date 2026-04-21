917e207685c109a78f4c1944a39ac4163ed7c32c
Johanes07 <johanesflorensius07@gmail.com>
2026-04-21 11:45:19 +0700

    update fitur user

--- app/Filament/Resources/Services/Pages/EditService.php
+++ app/Filament/Resources/Services/Pages/EditService.php
@@ -16,4 +16,8 @@ class EditService extends EditRecord
             DeleteAction::make(),
         ];
     }
+     protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
